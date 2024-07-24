c7661e99a81eb7947455c48d935b4469c8617a39
AniaPiWo <snowflake@wp.pl>
2024-07-24 14:46:58 +0200

    feat: upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import UploadPhoto from "../components/UploadPhoto";
 
 export default function Home() {
-  return <main className={styles.main}>app</main>;
+  return (
+    <main className={styles.main}>
+      app
+      <UploadPhoto />
+    </main>
+  );
 }
