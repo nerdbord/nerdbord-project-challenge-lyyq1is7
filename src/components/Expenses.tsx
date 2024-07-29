e1a8bb6561a4048a613bfc531cdcc9aa315cd07c
AniaPiWo <snowflake@wp.pl>
2024-07-29 11:05:36 +0200

    feat: swr

--- src/components/Expenses.tsx
+++ src/components/Expenses.tsx
@@ -1,58 +1,48 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { AddExpenseForm } from "./Form";
 import { useSupabaseClient } from "@/contexts/supabaseContext";
 import { useSession } from "@clerk/nextjs";
+import useSWR from "swr";
 
 interface Expense {
   id: number;
   name: string;
 }
 
+const fetcher = async (client: any) => {
+  const { data, error } = await client.from("expenses").select();
+  if (error) {
+    throw new Error("Failed to load expenses");
+  }
+  return data;
+};
+
 export const Expenses = () => {
-  const [expenses, setExpenses] = useState<Expense[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const { client } = useSupabaseClient();
   const { session } = useSession();
 
-  useEffect(() => {
-    if (client && session) {
-      loadExpenses();
-    }
-  }, [session, client]);
+  const {
+    data: expenses,
+    error,
+    mutate,
+  } = useSWR(client && session ? "expenses" : null, () => fetcher(client), {
+    revalidateOnFocus: false,
+  });
 
-  async function loadExpenses() {
-    setLoading(true);
-    setErrorMsg(null);
-
-    try {
-      const { data, error } = await client.from("expenses").select();
-      if (error) {
-        setErrorMsg("Failed to load expenses");
-      } else if (data.length === 0) {
-        setErrorMsg("No expenses found");
-      } else {
-        setExpenses(data);
-      }
-    } catch (error) {
-      setErrorMsg("Failed to load expenses");
-    } finally {
-      setLoading(false);
-    }
-  }
+  const isLoading = !expenses && !error;
 
   return (
     <div>
-      {loading && <p>Loading...</p>}
-      {!loading &&
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error.message}</p>}
+      {expenses && expenses.length === 0 && <p>No expenses found</p>}
+      {expenses &&
         expenses.length > 0 &&
-        !errorMsg &&
-        expenses.map((expense) => <p key={expense.id}>{expense.name}</p>)}
-      {!loading && errorMsg && <p>{errorMsg}</p>}
-
-      <AddExpenseForm client={client} onExpenseAdded={loadExpenses} />
+        expenses.map((expense: Expense) => (
+          <p key={expense.id}>{expense.name}</p>
+        ))}
+      <AddExpenseForm client={client} onExpenseAdded={() => mutate()} />
     </div>
   );
 };
