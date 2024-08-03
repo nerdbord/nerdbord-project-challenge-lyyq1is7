03794e31a38fb600d98f396bbfada1c44a33589c
AniaPiWo <snowflake@wp.pl>
2024-08-03 13:07:28 +0200

    feat: landing + capture screen

---
+++ src/components/DateFilter.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+interface DateFilterProps {
+  startDate: string;
+  endDate: string;
+  onStartDateChange: (date: string) => void;
+  onEndDateChange: (date: string) => void;
+}
+
+export const DateFilter: React.FC<DateFilterProps> = ({
+  startDate,
+  endDate,
+  onStartDateChange,
+  onEndDateChange,
+}) => {
+  return (
+    <div>
+      <label>
+        Start Date:
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => onStartDateChange(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        End Date:
+        <input
+          type="date"
+          value={endDate}
+          onChange={(e) => onEndDateChange(e.target.value)}
+        />
+      </label>
+    </div>
+  );
+};
